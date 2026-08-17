0de3e9b83a7c7d7e4fe210c5e0388b502a8f1999
Kenan Yildirim <kenan@kenany.me>
2026-08-17 17:27:37 -0400

    feat!: migrate to TypeScript
    BREAKING CHANGE: Named export and minimum Node.js version is now v22.

--- index.js
+++
@@ -1,3 +0,0 @@
-module.exports = Math.cosh || function cosh(x) {
-  return (Math.exp(x) + Math.exp(-x)) / 2;
-};
--- test/0-exports.js
+++
@@ -1,9 +0,0 @@
-const test = require('tape');
-const isFunction = require('lodash.isfunction');
-
-const cosh = require('../');
-
-test('exports a function', function(t) {
-  t.plan(1);
-  t.ok(isFunction(cosh));
-});
